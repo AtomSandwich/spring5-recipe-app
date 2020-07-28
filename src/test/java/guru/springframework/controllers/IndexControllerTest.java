d9e10015d0fbef016afa7817a6b57631ae325bd3
Adam Samek <borg166@gmail.com>
2020-07-28 17:56:58 -0400

    Added Spring Mock MVC to Test Controller

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import static org.mockito.Mockito.*;
@@ -16,6 +18,10 @@ import java.util.Set;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;;
 
 public class IndexControllerTest {
 
@@ -35,26 +41,33 @@ public class IndexControllerTest {
 	}
 
 	@Test
+	public void testMockMVC() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+	}
+
+	@Test
 	public void testGetIndexPage() {
 
 		// given
 		Set<Recipe> recipes = new HashSet<>();
 
 		recipes.add(new Recipe());
-		
+
 		Recipe recipe = new Recipe();
 		recipe.setId(1L);
-		
+
 		recipes.add(recipe);
 
 		when(recipeService.getRecipes()).thenReturn(recipes);
-		
+
 		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
-		//when
+		// when
 		String viewName = controller.getIndexPage(model);
 
-		//then
+		// then
 		assertEquals("index", viewName);
 		verify(recipeService, times(1)).getRecipes();
 		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
